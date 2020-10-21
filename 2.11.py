f9e93937394bd3f73d8b9ae925458d1576d3bdd7
Kamil Nagórzański <knagorzanski@gmail.com>
2020-10-21 19:49:41 +0200

    test

--- 2.11.py
+++ 2.11.py
@@ -3,4 +3,4 @@ newWord = ""
 for item in word:
     newWord += item + "_"
 newWord = newWord[:-1] #Usunięcie podkreślenia po ostatnim znaku, żeby podkreślenia znajdowały się tylko pomiędzy nimi
-print(newWord)  #wypisanie nowego słowa, gdzie znaki oddzielone są podkreśleniem
+print(newWord)  #wypisanie nowego słowa, gdzie znaki oddzielone są podkresleniem
