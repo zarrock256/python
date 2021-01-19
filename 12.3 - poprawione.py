4d4c6101c82b8977beff48fcbbe15d5900af0559
zarrock256 <61661076+zarrock256@users.noreply.github.com>
2021-01-19 17:05:10 +0100

    Zestaw12 - poprawa

---
+++ 12.3 - poprawione.py
@@ -0,0 +1,26 @@
+#Zaimplementować prostą metodę znajdowania mediany polegającą na posortowaniu zbioru i wybraniu elementu środkowego.
+
+import random
+import math
+
+n = 5
+k = 10
+L = []
+
+def mediana_sort(L1, left, right):
+    L = []
+    for i in range(left,right+1):
+        L.append(L1[i])
+    L.sort()
+    print(L)
+    rightL = len(L)-1
+    if len(L)%2 == 0:
+        mid = (0+rightL)/2
+        return (L[math.floor(mid)] + L[math.ceil(mid)])/2
+    return L[(0+rightL)//2]
+
+for i in range(n):
+    L.append(random.randint(0,k-1))
+
+print(L)
+print(mediana_sort(L,2,4))
